0269d38af38bc43986eb6b6e91d85b9003ba2001
Jared Grove <jaredkgrove@gmail.com>
2019-03-22 02:50:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,9 @@ class Song
     
   def self.genre_count
      genre_hash = Hash.new 0
-     @@genres.each {|genre| genre_hash[genre] += 1]
+     @@genres.each do |genre| 
+       genre_hash[genre] += 1
+     end
      genre_hash
   end
   
