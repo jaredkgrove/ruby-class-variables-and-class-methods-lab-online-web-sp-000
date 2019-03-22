e20630861fdd2ce0131937b6ad196a9875ab351c
Jared Grove <jaredkgrove@gmail.com>
2019-03-22 02:33:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     
     @@count += 1
     @@artists << name if !@@artists.include?(name)
-    @@genres << name if !@@genres.include?(name)
+    @@genres << genre
   end
   
   def self.count
